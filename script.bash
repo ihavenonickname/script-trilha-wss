f6a686ba55a5524d7492832eab3f7231f6923dd5
Gabriel <gbsm1993@gmail.com>
2023-07-31 11:07:31 -0300

    usar arquivo de output como input do proximo passo

--- script.bash
+++ script.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Considerações gerais sobre o script
-#
-#
-#
+
+
+################################################################################
+#                     CONSIDERAÇÕES GERAIS SOBRE O SCRIPT                      #
+################################################################################
+
 # Sobre bash vs sh
 #
 # Este script utiliza o bash para ser executado. O bash é um superset do sh,
@@ -13,7 +15,6 @@
 # - Arrays
 # - Expressões booleanas complexas
 # - Funções
-#
 #
 #
 # Sobre o uso de subshell
@@ -36,7 +37,6 @@
 # contrabarras.
 #
 #
-#
 # Sobre o descarte do stderr
 #
 # A sintaxe `2> /dev/null` é utilizada para ignorar o stderr dos comandos.
@@ -46,133 +46,204 @@
 # serem processados pelo computador.
 # Aqui só interessa o texto estruturado que é processado pela pipeline.
 
-# Este script deve receber no primeiro parâmetro o domínio base que será usado
-# na pipeline de recon.
-root_domain="$1"
+
+
+################################################################################
+#                             PROCESSAR ARGUMENTOS                             #
+################################################################################
 
 # A data e hora UTC em formato compatível com ISO 8601 é utilizada no nome dos
 # arquivos que contêm o output intermediário de cada passo.
 start_date=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
 
+# Domínio base que é informado através do argumento -d.
+root_domain=""
+
+# Itera por todos os argumentos passados pro script.
+while getopts "d:" opt;
+do
+    case $opt in
+        d) # O domínio base foi informado
+            root_domain="$OPTARG"
+            ;;
+        :) # Um argumento não teve nenhum valor informado
+            exit 1
+            ;;
+        \?) # Argumento não reconhecido
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $root_domain ];
+then
+    echo "Root domain cannot be empty" >&2;
+    exit 1;
+fi
+
+function get_output_filename
+{
+    echo "script-$start_date-step-$1.txt"
+}
+
+echo "Root domain: $root_domain"
+
+
+
+################################################################################
+#                      PASSO 1 - ENUMERAR OS SUBDOMÍNIOS                       #
+################################################################################
+
+echo "Step 1 - Enumerate subdomains"
+
+step_1_start_ts=$(date +%s)
+
+# Neste passo várias fontes públicas são consultadas para encontrar os
+# subdomínios abaixo do domínio base recebido por parâmetro. Nenhuma
+# interação com o alvo é feita neste passo.
+
+# Como duas ferramentas de enumeração são utilizadas neste passo, é preciso
+# criar um  subshell para concatenar o output de ambas as ferramentas,
+# permitindo que os demais comandos consigam processar cada subdomínio em
+# uma pipeline única.
 (
-    # Passo 1 - Enumerar os subdomínios.
+    # Flags utilizadas no amass
+    # -passive: Apenas consultar as fontes públicas sem interagir com o alvo.
+    # -d <string>: Domínio base a ser enumerado.
+    amass enum -passive -d $root_domain 2> /dev/null;
+
+    # Flags utilizadas no subfinder
+    # -silent: Não printar nada no stderr.
+    # -d <string>: Domínio base a ser enumerado.
     #
-    # Neste passo várias fontes públicas são consultadas para encontrar os
-    # subdomínios abaixo do domínio base recebido por parâmetro. Nenhuma
-    # interação com o alvo é feita neste passo.
-
-    # Como duas ferramentas de enumeração são utilizadas neste passo, é preciso
-    # criar um  subshell para concatenar o output de ambas as ferramentas,
-    # permitindo que os demais comandos consigam processar cada subdomínio em
-    # uma pipeline única.
-    (
-        # Flags utilizadas no amass
-        # -passive: Apenas consultar as fontes públicas sem interagir com o alvo.
-        # -d <string>: Domínio base a ser enumerado.
-        amass enum -passive -d $root_domain 2> /dev/null;
-
-        # Flags utilizadas no subfinder
-        # -silent: Não printar nada no stderr.
-        # -d <string>: Domínio base a ser enumerado.
-        #
-        # O subfinder não precisa de uma flag -passive como o amass porque a
-        # enumeração do subfinder sempre é passiva.
-        subfinder -silent -d $root_domain
-    ) | (
-        # Como as ferramentas de enumeração consultam várias fontes em comum, a
-        # vasta maioria dos subdomínios encontrados são repetidos. Por conta
-        # disso é necessário remover os duplicados.
-        #
-        # O comando sort ordena as linhas recebidas no stdin de forma
-        # lexicográfica e ascendente.
-        sort
-    ) | (
-        # O comando uniq remove linhas duplicadas que estejam em posições
-        # adjacentes. É por este motivo que o comando sort é chamado antes do
-        # uniq.
-        uniq
-    ) | (
-        # Ao fim deste passo, o formato do stdout é:
-        # mail.example.com
-        # qa.example.com
-        # www.example.com
-        tee "script-$start_date-step-1.txt"
-    )
+    # O subfinder não precisa de uma flag -passive como o amass porque a
+    # enumeração do subfinder sempre é passiva.
+    subfinder -silent -d $root_domain
 ) | (
-    # Passo 2 - Obter a URL completa.
+    # Como as ferramentas de enumeração consultam várias fontes em comum, a
+    # vasta maioria dos subdomínios encontrados são repetidos. Por conta
+    # disso é necessário remover os duplicados.
     #
-    # Faz uma requisição HTTP para cada subdomínio a fim de descobrir o
-    # protocolo utilizado (http ou https) e o status code. É importante notar
-    # que este comando gera ruído no alvo. Até aqui a pipeline fez apenas
-    # reconhecimento passivo, a partir deste ponto se torna reconhecimento
-    # ativo.
-
-    (
-        # Flags utilizadas no httpx-toolkit
-        # -sc: Printa o status code da requisição.
-        #
-        # Este comando ignora automaticamente domínios não-responsivos.
-        httpx-toolkit -sc 2> /dev/null
-    ) | (
-        # Flags utilizadas no grep
-        # -v: Inverte a captura. Ou seja, printa somente as linhas que _não_
-        # contêm a regex.
-        grep -v "\[404]"
-    ) | (
-        # Flags utilizadas no cut
-        # -d <string>: Informa o delimitador para fatiar a string.
-        # -f <int>: Informa o índice (iniciando em 1) da fatia selecionada.
-        cut -d ' ' -f 1
-    ) | (
-        # Ao fim deste passo, o formato do stdout é:
-        # https://mail.example.com
-        # https://www.example.com
-        tee "script-$start_date-step-2.txt"
-    )
+    # O comando sort ordena as linhas recebidas no stdin de forma
+    # lexicográfica e ascendente.
+    sort
 ) | (
-    # Passo 3 - Enumerar os diretórios.
-    #
-    # Aplica um brute-force em cada URL a fim de encontrar páginas acessíveis e
-    # possivelmente vulneráveis.
-
-    (
-        # O comando dirb testa uma lista de diretórios em uma dada URL. Caso
-        # nenhuma lista de diretórios seja informada (como é o caso aqui) ele
-        # utiliza a lista padrão que possui vários diretórios comuns em
-        # servidores web.
-        #
-        # Este comando espera receber a URL no primeiro parâmetro posicional,
-        # ou seja, não é possível encadear o dirb usando pipes. Por este motivo,
-        # o comando xargs atua como um "wraper" que redireciona cada linha do
-        # stdin para o primeiro parâmetro do dirb.
-        #
-        # Flags utilizadas no xargs
-        # -I <string>: Informa o placeholder que será substituido pela linha
-        # vinda do stdin.
-        #
-        # Flags utilizadas no dirb
-        # -S: Modo silencioso. Não mexe na posição do cursor.
-        xargs -I {} dirb {} -S
-    ) | (
-        # Como o output do dirb mistura texto em inglês, marcadores e outras
-        # coisas, é necessário aplicar o grep e o cut para extrair as URLs que
-        # realmente interessam.
-        #
-        # Flags utilizadas no grep
-        # -E: Iutilizar o motor de regex estendido. Necessário por conta da
-        # âncora de início da string.
-        grep -E '^==> DIRECTORY:'
-    ) | (
-        # Flags utilizadas no cut
-        # -d <string>: Informa o delimitador para fatiar a string.
-        # -f <int>: Informa o índice (iniciando em 1) da fatia selecionada.
-        cut -d ' ' -f 3
-    ) | (
-        # Ao fim deste passo, o formato do stdout é:
-        # https://mail.example.com/admin
-        # https://mail.example.com/files
-        # https://www.example.com/cpanel
-        # https://www.example.com/wwwroot/static
-        tee "script-$start_date-step-3.txt"
-    )
+    # O comando uniq remove linhas duplicadas que estejam em posições
+    # adjacentes. É por este motivo que o comando sort é chamado antes do
+    # uniq.
+    uniq
+) | (
+    # Ao fim deste passo, o formato do stdout é:
+    # mail.example.com
+    # qa.example.com
+    # www.example.com
+    cat > $(get_output_filename 1)
 )
+
+step_1_end_ts=$(date +%s)
+step_1_elapsed_seconds=$((step_1_end_ts - step_1_start_ts))
+
+echo "Step 1 - Finished in $step_1_elapsed_seconds seconds"
+
+
+
+################################################################################
+#                         PASSO 2 - OBTER AS URLS ATIVAS                       #
+################################################################################
+
+echo "Step 2 - Get the active URLs"
+
+step_2_start_ts=$(date +%s)
+
+# Faz uma requisição HTTP para cada subdomínio a fim de descobrir o
+# protocolo utilizado (http ou https) e o status code. É importante notar
+# que este comando gera ruído no alvo. Até aqui a pipeline fez apenas
+# reconhecimento passivo, a partir deste ponto se torna reconhecimento
+# ativo.
+
+cat $(get_output_filename 1) | (
+    # Flags utilizadas no httpx-toolkit
+    # -sc: Printa o status code da requisição.
+    #
+    # Este comando ignora automaticamente domínios não-responsivos.
+    httpx-toolkit -sc 2> /dev/null
+) | (
+    # Flags utilizadas no grep
+    # -v: Inverte a captura. Ou seja, printa somente as linhas que _não_
+    # contêm a regex.
+    grep -v "\[404]"
+) | (
+    # Flags utilizadas no cut
+    # -d <string>: Informa o delimitador para fatiar a string.
+    # -f <int>: Informa o índice (iniciando em 1) da fatia selecionada.
+    cut -d ' ' -f 1
+) | (
+    # Ao fim deste passo, o formato do stdout é:
+    # https://mail.example.com
+    # https://www.example.com
+    cat > $(get_output_filename 2)
+)
+
+step_2_end_ts=$(date +%s)
+step_2_elapsed_seconds=$((step_2_end_ts - step_2_start_ts))
+
+echo "Step 2 - Finished in $step_2_elapsed_seconds seconds"
+
+
+
+################################################################################
+#                       PASSO 3 - ENUMERAR OS DIRETÓRIOS                       #
+################################################################################
+
+echo "Step 3 - Enumerate directories"
+
+step_3_start_ts=$(date +%s)
+
+# Aplica um brute-force em cada URL a fim de encontrar páginas acessíveis e
+# possivelmente vulneráveis.
+
+cat $(get_output_filename 2) | (
+    # O comando dirb testa uma lista de diretórios em uma dada URL. Caso
+    # nenhuma lista de diretórios seja informada (como é o caso aqui) ele
+    # utiliza a lista padrão que possui vários diretórios comuns em
+    # servidores web.
+    #
+    # Este comando espera receber a URL no primeiro parâmetro posicional,
+    # ou seja, não é possível encadear o dirb usando pipes. Por este motivo,
+    # o comando xargs atua como um "wraper" que redireciona cada linha do
+    # stdin para o primeiro parâmetro do dirb.
+    #
+    # Flags utilizadas no xargs
+    # -I <string>: Informa o placeholder que será substituido pela linha
+    # vinda do stdin.
+    #
+    # Flags utilizadas no dirb
+    # -S: Modo silencioso. Não mexe na posição do cursor.
+    xargs -I {} dirb {} -S
+) | (
+    # Como o output do dirb mistura texto em inglês, marcadores e outras
+    # coisas, é necessário aplicar o grep e o cut para extrair as URLs que
+    # realmente interessam.
+    #
+    # Flags utilizadas no grep
+    # -E: Iutilizar o motor de regex estendido. Necessário por conta da
+    # âncora de início da string.
+    grep -E '^==> DIRECTORY:'
+) | (
+    # Flags utilizadas no cut
+    # -d <string>: Informa o delimitador para fatiar a string.
+    # -f <int>: Informa o índice (iniciando em 1) da fatia selecionada.
+    cut -d ' ' -f 3
+) | (
+    # Ao fim deste passo, o formato do stdout é:
+    # https://mail.example.com/admin
+    # https://mail.example.com/files
+    # https://www.example.com/cpanel
+    # https://www.example.com/wwwroot/static
+    cat > $(get_output_filename 3)
+)
+
+step_3_end_ts=$(date +%s)
+step_3_elapsed_seconds=$((step_3_end_ts - step_3_start_ts))
+
+echo "Step 3 - Finished in $step_3_elapsed_seconds seconds"
